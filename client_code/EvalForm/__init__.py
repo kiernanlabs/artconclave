1ff12e53c2ff889fca90b39b30ddc53c3fb25619
joey32@gmail.com <joey32@gmail.com>
2025-03-24 00:16:59 +0000

    Created EvalForm; Edited settings; Deleted eval

--- client_code/EvalForm/__init__.py
+++ client_code/EvalForm/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import evalTemplate
+from ._anvil_designer import EvalFormTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -6,7 +6,7 @@ from anvil.tables import app_tables
 import anvil.server
 
 
-class EvalForm(evalTemplate):
+class EvalForm(EvalFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -14,6 +14,6 @@ class EvalForm(evalTemplate):
     # Any code you write here will run before the form opens.
 
   def submit_button_click(self, **event_args):
-    artist = self.artist_box    
-    file = self.file_input
-    anvil.server.store_artwork(file,artist)
+    artist = self.artist_box.text    
+    file = self.file_input.file
+    anvil.server.call('store_artwork',file,artist)
